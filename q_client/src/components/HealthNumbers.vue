8d207b780137c7ccc696e0a7b58238cb1fd896b6
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-04-18 11:52:25 -0700

    Example Chart

--- q_client/src/components/HealthNumbers.vue
+++ q_client/src/components/HealthNumbers.vue
@@ -1,16 +1,36 @@
 <template>
   <div class="main-content">
     Health Numbers
+    <div class="layout-view">
+      <something :data="data"></something>
+    </div>
   </div>
 </template>
 
 <script>
+  import Something from './Something'
   export default {
+    components: {
+      Something
+    },
     props: ['setupContent'],
     created () {
       this.setupContent({
         title: 'Status'
       })
+    },
+    data () {
+      return {
+        labels: ['a', 'b', 'c'],
+        datasets: [{
+          label: ['one', 'two', 'three'],
+          backgroundColor: '#ff00ee',
+          data: [10, 20, 16]
+        }],
+        option: {
+          height: 100
+        }
+      }
     }
   }
 </script>
