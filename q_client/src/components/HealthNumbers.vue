0b5e0db1636667abe0d6f21e881bfea13e35cc18
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-04-23 16:29:53 -0700

    First API-Mobile Chart Connection with PostgREST

--- q_client/src/components/HealthNumbers.vue
+++ q_client/src/components/HealthNumbers.vue
@@ -2,36 +2,67 @@
   <div class="main-content">
     Health Numbers
     <div class="layout-view">
-      <something :data="data"></something>
+      <line-chart :chart-data="chartData" ref="lineChart" :options="chartOptions" :height="chartOptions.height"></line-chart>
     </div>
+
   </div>
 </template>
 
 <script>
-  import Something from './Something'
+  import LineChart from './LineChart'
+
   export default {
     components: {
-      Something
+      LineChart
     },
     props: ['setupContent'],
     created () {
       this.setupContent({
         title: 'Status'
       })
+
+      this.$http.get(this.$root.$options.apiHost + '/a_user_medical_state?measurement=eq.blood_pressure&order=created', {
+        // 'measurement_0': 'blood_pressure'
+      }).then(response => {
+        let fetchLabels = d => d.created.slice(0, 10)
+        let fetchSystolic = d => d.data['systolic']
+        let fetchDiastolic = d => d.data['diastolic']
+
+        this.chartData.labels = response.data.map(fetchLabels)
+        this.chartData.datasets[0].data = response.data.map(fetchSystolic)
+        this.chartData.datasets[1].data = response.data.map(fetchDiastolic)
+
+        this.$refs.lineChart.update()
+      })
     },
     data () {
       return {
-        labels: ['a', 'b', 'c'],
-        datasets: [{
-          label: ['one', 'two', 'three'],
-          backgroundColor: '#ff00ee',
-          data: [10, 20, 16]
-        }],
-        option: {
-          height: 100
+        chartData: {
+          labels: [],
+          datasets: [
+            {
+              label: 'Systolic',
+              backgroundColor: '#00cc99',
+              data: [],
+              fill: false,
+              borderColor: '#00cc99'
+            },
+            {
+              label: 'Diastolic',
+              backgroundColor: '#ffcc0f',
+              data: [],
+              fill: false,
+              borderColor: '#ffcc0f'
+            }
+          ]
+        },
+        chartOptions: {
+          height: 150,
+          backgroundColor: '#ff0000',
+          label: 'Blood Pressure',
+          fill: false
         }
       }
     }
   }
 </script>
-
