1be5628db56198507ea95a8472824de45347db32
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-05-31 16:59:46 -0700

    Chat Placeholder

--- q_client/src/components/Chat.vue
+++ q_client/src/components/Chat.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="main-content">
-    Chat
+    <!--<q-page padding class="row justify-center">-->
+      <div style="width: 500px; max-width: 90vw;">
+        <q-chat-message
+          v-for="(msg, index) in messages"
+          :key="`reg-${index}`"
+          :label="msg.label"
+          :sent="msg.sent"
+          :text-color="msg.textColor"
+          :bg-color="msg.bgColor"
+          :name="msg.name"
+          :avatar="msg.avatar"
+          :text="msg.text"
+          :stamp="msg.stamp"
+        />
+        <q-chat-message
+          name="Maggy"
+          avatar="statics/grandma-avatar.png"
+        >
+          <q-spinner-dots size="2rem" />
+        </q-chat-message>
+      </div>
+
+      <div style="width: 500px; max-width: 90vw; padding: 20px;">
+        <q-input type="textarea" name="new-comment" placeholder="Message" />
+        <q-btn class="action-btn" side="right" color="primary">Send</q-btn>
+      </div>
+    <!--</q-page>-->
   </div>
 </template>
 
@@ -12,6 +38,66 @@
       this.setupContent({
         title: 'Chat'
       })
+    },
+    data () {
+      return {
+        message: '',
+        messages: [
+          {
+            label: 'Friday, 18th'
+          },
+          {
+            name: 'Maggy',
+            text: ['How are you?'],
+            avatar: 'statics/grandma-avatar.png',
+            stamp: 'Yesterday 13:34'
+          },
+          {
+            name: 'John',
+            text: ['I\'m good, mom!', 'How is your day?'],
+            sent: true,
+            avatar: 'statics/man-avatar.png',
+            stamp: 'Yesterday at 13:50'
+          },
+          {
+            name: 'Maggy',
+            text: ['Perfect!', 'Caressa is so cool that we can keep up with little to no effort!'],
+            avatar: 'statics/grandma-avatar.png',
+            stamp: 'Yesterday at 13:51'
+          },
+          {
+            name: 'John',
+            text: ['I know ma!', 'I love you.'],
+            sent: true,
+            avatar: 'statics/man-avatar.png',
+            stamp: 'Yesterday at 13:50'
+          },
+
+          {
+            label: 'Sunday, 20th'
+          },
+          {
+            name: 'John',
+            text: ['Nice weather here today. How is it over there?'],
+            sent: true,
+            avatar: 'statics/man-avatar.png',
+            stamp: 'Yesterday at 13:50'
+          },
+          {
+            name: '',
+            text: ['Pretty good.', 'By the way, my caregiver is with me now. We\'ll take a walk now'],
+            avatar: 'statics/grandma-avatar.png',
+            stamp: 'Yesterday at 13:50'
+          },
+          {
+            name: 'John',
+            text: ['That\'s great! Talk to you later mom'],
+            sent: true,
+            avatar: 'statics/man-avatar.png',
+            stamp: 'Yesterday at 13:50'
+          }
+        ]
+      }
     }
   }
 </script>
