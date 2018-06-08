4c309210097c0795cd3fb320401b485893f13aba
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-06-07 17:21:32 -0700

    Start of Post Master/Detail-Activity

--- q_client/src/components/Post.vue
+++ q_client/src/components/Post.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="main-content">
-    Post
+    <post-detail-activity v-if="selected=='activity'"></post-detail-activity>
+    <div v-else>
+      <div class="row">
+        Master Page is here...
+      </div>
+      <div class="row">
+        <q-btn outline color="primary col-5" @click="selected='activity'" label="Activity">Activity</q-btn>
+        <q-btn outline color="primary col-5" @click="selected='activity'" label="Something Else">Something Else</q-btn>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import PostDetailActivity from './PostDetailActivity'
+
   export default {
     name: 'post',
     props: ['setupContent'],
+    components: {
+      PostDetailActivity
+    },
     created () {
       this.setupContent({
         title: 'Post'
       })
+    },
+    data () {
+      return {
+        selected: null
+      }
     }
   }
 </script>
