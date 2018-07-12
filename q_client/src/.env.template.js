094f5b0152abe49753c5134e28ea33dd5d159f2f
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-07-12 01:07:23 -0700

    Combining environment specific variables together

---
+++ q_client/src/.env.template.js
@@ -0,0 +1,10 @@
+// copy and paste this file as .env.js and
+// make the necessary changes according to README file
+
+export default {
+  ENV: 'dev',
+  PUSHER_CHANNEL: 'pusher-channel-name',   // todo hard coded here
+  PUSHER_KEY: 'key-value',
+  PUSHER_CLUSTER: 'cluster-value',
+  API_HOST: 'api-host-address'
+}
