054736e0c2e0060696f1bac9f3f07e98abd16e41
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2018-07-25 01:05:27 -0700

    JSON Field Display with Widget in Fact Admin Page

--- alexa/admin.py
+++ alexa/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from alexa.models import Fact, FactType
+# from django.db import models
+from django import forms
+from utilities.widgets.split_json_widget import SplitJSONWidget
+from jsonfield import JSONField
 
-# Register your models here.
+
+@admin.register(Fact)
+class FactAdmin(admin.ModelAdmin):
+    def upper_case_entry_text(self, obj):
+        return obj.entry_text.upper()
+
+    upper_case_entry_text.short_description = 'Entry Text Ho'
+    upper_case_entry_text.admin_order_field = 'entry_text'
+
+    fields = ('fact_type', 'entry_text', 'fact_list', 'ending_yes_no_question', )
+    list_display = ('fact_type', 'entry_text', 'upper_case_entry_text', )
+
+    formfield_overrides = {
+        JSONField: {'widget': SplitJSONWidget(debug=False)}
+    }
+
+
+@admin.register(FactType)
+class FactTypeAdmin(admin.ModelAdmin):
+    pass
