be8a9a904314abf42c75b37aa16c69528e1d70f3
MikailSeremet <mikail.seremet@gmail.com>
2019-01-29 14:32:35 -0800

    Password Reset Templates

--- senior_living_facility/urls.py
+++ senior_living_facility/urls.py
@@ -17,7 +17,7 @@ urls = [
     path('invitation/', family_prospect_invitation, name='family-prospect-invitation-code'),
     path('password_reset/', auth_views.password_reset, name='password_reset'),
     path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
-    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+    path('reset/<uidb64>/<token>/',
          auth_views.password_reset_confirm, name='password_reset_confirm'),
     path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete')
 ]
