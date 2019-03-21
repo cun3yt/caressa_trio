3e524ee70e216c40c36230d8bd10b04e75431754
MikailSeremet <mikail.seremet@gmail.com>
2019-03-20 19:36:59 -0700

    Tests for Tag and AudioFile Old test for Tag and AudioFile become obsolete after changes. Removed mommy_recipes file as it creates some inconsistency across the other cases.

--- streaming/tests.py
+++ streaming/tests.py
@@ -1,96 +1,111 @@
+from uuid import uuid4
+
 from django.test import TestCase
 from mock import patch
 from model_mommy import mommy
+from model_mommy.recipe import seq
+
+from utilities.logger import log
+from alexa.models import User, UserSettings
 from streaming.models import Tag, AudioFile, audio_file_accessibility_and_duration
 # from streaming.views import stream_io
 from django.db.models import signals
-import datetime
 import boto3
-from random import randint
 # from streaming.test_helper_functions import request_body_creator_for_next_command, request_body_creator_for_intent, \
 #     request_body_creator, request_body_creator_for_audio_player, request_body_creator_for_pause_command
-# import botocore
+from rest_framework.test import APIRequestFactory
 
 
 class TagModelTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         signals.pre_save.disconnect(receiver=audio_file_accessibility_and_duration,
-                                    sender=AudioFile, dispatch_uid='audio_file_accessibility_and_duration')
+                                    sender=AudioFile,
+                                    dispatch_uid='audio_file_accessibility_and_duration')
 
-        tag1 = mommy.make(Tag, name='song-classical')
-        cls.user_one = mommy.make_recipe('alexa.user')
-        cls.audio_file1 = mommy.make_recipe('streaming.audio_file_recipe')  # type: AudioFile
-        cls.audio_file1.tags.add(tag1)
+        cls.tag1 = mommy.make('streaming.Tag', name='song-classical', is_setting_available=False)  # type: Tag
+        cls.default_tag1 = mommy.make('streaming.Tag', is_setting_available=True)  # type: Tag
+        cls.default_tag2 = mommy.make('streaming.Tag', is_setting_available=True)   # type: Tag
 
-        tag_irrelevant = mommy.make(Tag, name='irrelevant')
-        cls.audio_file2 = mommy.make_recipe('streaming.audio_file_recipe')  # type: AudioFile
-        cls.audio_file3 = mommy.make_recipe('streaming.audio_file_recipe')  # type: AudioFile
+        cls.audio_file1 = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file1.tags.add(cls.tag1)
+
+        tag_irrelevant = mommy.make('streaming.Tag', name='irrelevant', is_setting_available=False)  # type: Tag
+        cls.audio_file2 = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file3 = mommy.make('streaming.AudioFile')  # type: AudioFile
 
         cls.audio_file2.tags.add(tag_irrelevant)
         cls.audio_file3.tags.add(tag_irrelevant)
 
-        tag_multi1 = mommy.make(Tag, name='song-tag-1')
-        tag_multi2 = mommy.make(Tag, name='song-tag-2')
+        cls.tag_multi1 = mommy.make('streaming.Tag', name='song-tag-1', is_setting_available=False)  # type: Tag
+        cls.tag_multi2 = mommy.make('streaming.Tag', name='song-tag-2', is_setting_available=False)  # type: Tag
 
-        cls.audio_file_multi_tagged = mommy.make_recipe('streaming.audio_file_recipe')  # type: AudioFile
-        cls.audio_file_multi_tagged.tags.add(tag_multi1)
-        cls.audio_file_multi_tagged.tags.add(tag_multi2)
+        cls.audio_file_multi_tagged = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file_multi_tagged.tags.add(cls.tag_multi1)
+        cls.audio_file_multi_tagged.tags.add(cls.tag_multi2)
 
-        cls.same_tag_audio_file1 = mommy.make_recipe('streaming.audio_file_recipe')     # type: AudioFile
-        cls.same_tag_audio_file2 = mommy.make_recipe('streaming.audio_file_recipe')     # type: AudioFile
+        cls.same_tag_audio_file1 = mommy.make('streaming.AudioFile')     # type: AudioFile
+        cls.same_tag_audio_file2 = mommy.make('streaming.AudioFile')     # type: AudioFile
 
-        tag_for_multi_audio = mommy.make(Tag, name='multi-audio')
-        tag_for_only_first = mommy.make(Tag, name='multi-audio-but-only-first')
+        cls.tag_for_multi_audio = mommy.make('streaming.Tag',                # type: Tag
+                                             name='multi-audio',
+                                             is_setting_available=False)
+        cls.tag_for_only_first = mommy.make('streaming.Tag',                 # type: Tag
+                                            name='multi-audio-but-only-first',
+                                            is_setting_available=False)
 
-        cls.same_tag_audio_file1.tags.add(tag_for_multi_audio)
-        cls.same_tag_audio_file1.tags.add(tag_for_only_first)
-        cls.same_tag_audio_file2.tags.add(tag_for_multi_audio)
+        cls.same_tag_audio_file1.tags.add(cls.tag_for_multi_audio)
+        cls.same_tag_audio_file1.tags.add(cls.tag_for_only_first)
+        cls.same_tag_audio_file2.tags.add(cls.tag_for_multi_audio)
 
-    # def test_tag_string_to_audio_file(self):
-    #     tag_id = '2'
-    #     audio_file_fetched = Tag.string_to_audio_file(tag_id)
-    #     self.assertEqual(audio_file_fetched, self.audio_file1)
-    #
-    #     tag_id2 = '2, 3, 4'
-    #     audio_file_fetched = Tag.string_to_audio_file(tag_id2)
-    #     self.assertEqual(audio_file_fetched, self.audio_file1)
-    #
-    #     tag_id3 = '2, 3, 4'
-    #     audio_file_fetched = Tag.string_to_audio_file(tag_id3)
-    #     self.assertEqual(audio_file_fetched, self.audio_file1)
-    #
-    # def test_tag_string_to_not_none(self):
-    #     tag_str =
-    #     audio_file_fetched = Tag.string_to_audio_file(tag_str)
-    #     self.assertIsNotNone(audio_file_fetched, 'If no matching tag in audio files it leads to None')
-    #
-    # def test_tag_string_to_audio_with_multiple_tag(self):
-    #     audio_file_fetched = Tag.string_to_audio_file('2')
-    #     self.assertEqual(audio_file_fetched, self.audio_file_multi_tagged)
-    #
-    #     audio_file_fetched2 = Tag.string_to_audio_file('2, 2, 3, 4, 5')
-    #     self.assertEqual(audio_file_fetched2, self.audio_file_multi_tagged)
-    #
-    #     audio_file_fetched3 = Tag.string_to_audio_file('5, 4, 3, 2')
-    #     self.assertEqual(audio_file_fetched3, self.audio_file_multi_tagged)
-    #
-    #     audio_file_fetched4 = Tag.string_to_audio_file('3, 2, 4, 5, 6, 7')
-    #     self.assertEqual(audio_file_fetched4, self.audio_file_multi_tagged)
-    #
-    #     audio_file_fetched5 = Tag.string_to_audio_file('3, 2')
-    #     self.assertEqual(audio_file_fetched5, self.audio_file_multi_tagged)
-    #
-    #     audio_file_fetched6 = Tag.string_to_audio_file('2, 3, 4')
-    #     self.assertIsNone(audio_file_fetched6)
+        cls.audio_file_default_tagged = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file_default_tagged_2 = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file_default_tagged_3 = mommy.make('streaming.AudioFile')  # type: AudioFile
+        cls.audio_file_default_tagged.tags.add(cls.default_tag1)
+        cls.audio_file_default_tagged_2.tags.add(cls.default_tag2)
+        cls.audio_file_default_tagged_3.tags.add(cls.default_tag1)
+        cls.audio_file_default_tagged_3.tags.add(cls.default_tag2)
 
-    # def test_same_tag_multi_audio(self):
-    #
-    #     audio_file_fetched = Tag.string_to_audio_file('6')
-    #     self.assertIn(audio_file_fetched, [self.same_tag_audio_file1, self.same_tag_audio_file2])
-    #
-    #     audio_file_fetched2 = Tag.string_to_audio_file('3')
-    #     self.assertEqual(audio_file_fetched2, self.same_tag_audio_file1)
+    def test_tag_list_to_audio_file(self):
+        tag_list = [self.tag1.id]
+        tag_list_2 = [self.tag1.id, 300, 400]
+        audio_file_fetched = Tag.tag_list_to_audio_file(tag_list)
+        audio_file_fetched_2 = Tag.tag_list_to_audio_file(tag_list_2)
+
+        self.assertTrue(self.audio_file1.tags.all().count() == 1)
+        self.assertEqual(audio_file_fetched, self.audio_file1)
+        self.assertEqual(audio_file_fetched_2, self.audio_file1)
+
+    def test_empty_tag_list_to_audio_file(self):
+        empty_tag_list = []
+        audio_file_fetched = Tag.tag_list_to_audio_file(empty_tag_list)
+        self.assertIn(audio_file_fetched, [self.audio_file_default_tagged,
+                                           self.audio_file_default_tagged_2,
+                                           self.audio_file_default_tagged_3])
+
+    def test_tag_string_to_audio_with_multiple_tag(self):
+        audio_file_fetched = Tag.tag_list_to_audio_file([self.tag_multi1.id])
+        audio_file_fetched_2 = Tag.tag_list_to_audio_file([self.tag_multi2.id])
+        audio_file_fetched3 = Tag.tag_list_to_audio_file([self.tag_multi1.id, self.tag_multi2.id])
+
+        self.assertTrue(self.audio_file_multi_tagged.tags.all().count() == 2)
+        self.assertEqual(audio_file_fetched, self.audio_file_multi_tagged)
+        self.assertEqual(audio_file_fetched_2, self.audio_file_multi_tagged)
+        self.assertEqual(audio_file_fetched3, self.audio_file_multi_tagged)
+
+    def test_same_tag_multi_audio(self):
+        audio_file_fetched = Tag.tag_list_to_audio_file([self.tag_for_multi_audio.id])
+        self.assertIn(audio_file_fetched, [self.same_tag_audio_file1, self.same_tag_audio_file2])
+
+        audio_file_fetched2 = Tag.tag_list_to_audio_file([self.tag_for_only_first.id])
+        self.assertEqual(audio_file_fetched2, self.same_tag_audio_file1)
+
+    def test_default_tags_list(self):
+        default_tag_list = Tag.default_tags_list()
+        created_default_tags = [self.default_tag1.id, self.default_tag2.id]
+
+        self.assertGreater(len(default_tag_list), 0)
+        self.assertEqual(default_tag_list, created_default_tags)
 
 
 class AudioFileModelTestCase(TestCase):
@@ -98,9 +113,14 @@ class AudioFileModelTestCase(TestCase):
     def setUpTestData(cls):
         signals.pre_save.disconnect(receiver=audio_file_accessibility_and_duration,
                                     sender=AudioFile, dispatch_uid='audio_file_accessibility_and_duration')
-        cls.audio_file1 = mommy.make_recipe('streaming.audio_file_recipe', duration=40,
-                                            name='song name')  # type: AudioFile
-        cls.audio_file2 = mommy.make_recipe('streaming.audio_file_recipe', duration=100)  # type: AudioFile
+        cls.audio_file1 = mommy.make('streaming.AudioFile',  # type: AudioFile
+                                     duration=40,
+                                     audio_type=AudioFile.TYPE_SONG,
+                                     name='song name'
+                                     )
+        cls.audio_file2 = mommy.make('streaming.AudioFile',  # type: AudioFile
+                                     duration=100,
+                                     )
 
     def test_url_hyperlink(self):
         created_html = "<a href='{url}' target='_blank'>{url}</a>".format(url=self.audio_file1.url)
@@ -121,7 +141,7 @@ class AudioFileModelTestCase(TestCase):
         signals.pre_save.connect(receiver=audio_file_accessibility_and_duration,
                                  sender=AudioFile,
                                  dispatch_uid='audio_file_accessibility_and_duration')
-        bucket_name = 'caressa-test-{random_number}'.format(random_number=randint(1, 100000))
+        bucket_name = 'caressa-test-{uuid}'.format(uuid=str(uuid4())[:8])
         s3 = boto3.client('s3')
         s3.create_bucket(Bucket=bucket_name)
         file_path = 'streaming/sample_files/sample_audio.mp3'
@@ -155,29 +175,38 @@ class AudioFileModelTestCase(TestCase):
         actual_string_representation = str(self.audio_file1)
         self.assertEqual(actual_string_representation, "(song) song name")
 
+
 # todo Read and enable tests: https://www.django-rest-framework.org/api-guide/testing/
-#
-# class StreamingPlayTestCase(TestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.play_list_has_audio_1 = mommy.make_recipe('streaming.playlist_has_audio_recipe')
-#         cls.playlist_2 = mommy.make_recipe('streaming.playlist_recipe_2')   # second time user2 recipe runs
-#
-#         cls.auser_2 = mommy.make(AUser,
-#                                  user=cls.playlist_2.user,
-#                                  alexa_user_id='TestAlexaUserId2',
-#                                  alexa_device_id='TestAlexaDeviceId2',
-#                                  engine_schedule='TestEngineSchedule', )
-#
-#         cls.audio_file_1 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_2 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_3 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.playlist_2.add_audio_file(cls.audio_file_1)
-#         cls.playlist_2.add_audio_file(cls.audio_file_2)
-#         cls.play_list_has_audio_1.playlist.add_audio_file(cls.audio_file_3)
-#         u = Playlist.objects.all()[0]  # to make sure default playlist have no user assigned
-#         u.user_id = None
-#         u.save()
+
+class StreamingPlayTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+
+        # Using the standard RequestFactory API to create a form POST request
+        # factory = APIRequestFactory()
+        # request = factory.post('/api/test/', {'title': 'new idea'})
+
+        cls.user = mommy.make(User)
+
+        cls.tag1 = mommy.make('streaming.Tag')
+        cls.tag2 = mommy.make('streaming.Tag')
+        cls.user_settings = mommy.make(UserSettings, user=cls.user, data={"genres": [cls.tag1.id, cls.tag2.id]})
+
+        # Create 3 different AudioFiles with different number of tags (0, 1, 2)
+        cls.audio_file_1 = mommy.make('streaming.AudioFile',
+                                      url=seq('http://www.example.com/audio/song.mp3'),
+                                      )
+        cls.audio_file_2 = mommy.make('streaming.AudioFile',
+                                      make_m2m=True,
+                                      tags=[cls.tag1, cls.tag2],
+                                      url=seq('http://www.example.com/audio/song.mp3'),
+                                      )
+        cls.audio_file_3 = mommy.make('streaming.AudioFile',
+                                      make_m2m=True,
+                                      tags=[cls.tag1],
+                                      url=seq('http://www.example.com/audio/song.mp3'),
+                                      )
+
 #
 #     def test_cold_start_launch_request(self):
 #         request_body = request_body_creator(True, 'LaunchRequest')
@@ -285,8 +314,8 @@ class AudioFileModelTestCase(TestCase):
 # class StreamingNextAndQueueTestCase(TestCase):
 #     @classmethod
 #     def setUpTestData(cls):
-#         cls.play_list_has_audio_1 = mommy.make_recipe('streaming.playlist_has_audio_recipe')
-#         cls.playlist_2 = mommy.make_recipe('streaming.playlist_recipe_2')  # second time user2 recipe runs
+#         cls.play_list_has_audio_1 = mommy.make('streaming.playlist_has_audio_recipe')
+#         cls.playlist_2 = mommy.make('streaming.playlist_recipe_2')  # second time user2 recipe runs
 #
 #         cls.auser_2 = mommy.make(AUser,
 #                                  user=cls.playlist_2.user,
@@ -294,9 +323,9 @@ class AudioFileModelTestCase(TestCase):
 #                                  alexa_device_id='TestAlexaDeviceId2',
 #                                  engine_schedule='TestEngineSchedule', )
 #
-#         cls.audio_file_1 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_2 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_3 = mommy.make_recipe('streaming.audio_file_recipe')
+#         cls.audio_file_1 = mommy.make('streaming.audio_file_recipe')
+#         cls.audio_file_2 = mommy.make('streaming.audio_file_recipe')
+#         cls.audio_file_3 = mommy.make('streaming.audio_file_recipe')
 #         cls.playlist_2.add_audio_file(cls.audio_file_1)
 #         cls.playlist_2.add_audio_file(cls.audio_file_2)
 #         cls.play_list_has_audio_1.playlist.add_audio_file(cls.audio_file_3)
@@ -445,8 +474,8 @@ class AudioFileModelTestCase(TestCase):
 # class StreamingPauseAndFillerTestCase(TestCase):
 #     @classmethod
 #     def setUpTestData(cls):
-#         cls.play_list_has_audio_1 = mommy.make_recipe('streaming.playlist_has_audio_recipe')
-#         cls.playlist_2 = mommy.make_recipe('streaming.playlist_recipe_2')  # second time user2 recipe runs
+#         cls.play_list_has_audio_1 = mommy.make('streaming.playlist_has_audio_recipe')
+#         cls.playlist_2 = mommy.make('streaming.playlist_recipe_2')  # second time user2 recipe runs
 #
 #         cls.auser_2 = mommy.make(AUser,
 #                                  user=cls.playlist_2.user,
@@ -454,9 +483,9 @@ class AudioFileModelTestCase(TestCase):
 #                                  alexa_device_id='TestAlexaDeviceId2',
 #                                  engine_schedule='TestEngineSchedule', )
 #
-#         cls.audio_file_1 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_2 = mommy.make_recipe('streaming.audio_file_recipe')
-#         cls.audio_file_3 = mommy.make_recipe('streaming.audio_file_recipe')
+#         cls.audio_file_1 = mommy.make('streaming.audio_file_recipe')
+#         cls.audio_file_2 = mommy.make('streaming.audio_file_recipe')
+#         cls.audio_file_3 = mommy.make('streaming.audio_file_recipe')
 #         cls.playlist_2.add_audio_file(cls.audio_file_1)
 #         cls.playlist_2.add_audio_file(cls.audio_file_2)
 #         cls.play_list_has_audio_1.playlist.add_audio_file(cls.audio_file_3)
