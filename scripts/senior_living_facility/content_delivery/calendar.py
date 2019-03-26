5557fc87d31b441cad6138546872eaddcdcef066
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2019-03-25 18:04:36 -0700

    Upcoming Hourly Event Announcement

--- scripts/senior_living_facility/content_delivery/calendar.py
+++ scripts/senior_living_facility/content_delivery/calendar.py
@@ -4,6 +4,7 @@ from alexa.models import User
 from typing import Union
 from caressa.settings import pusher_client
 from utilities.time import time_today_in_tz
+from datetime import timedelta
 
 
 errors = {
@@ -19,15 +20,38 @@ errors = {
 }
 
 
-def deliver_daily_calendar(facility: Union[SeniorLivingFacility, int, str]):
+def _get_facility(facility: Union[SeniorLivingFacility, int, str]) -> SeniorLivingFacility:
     if isinstance(facility, str):
         facility = int(facility)
-
     if isinstance(facility, int):
         facility = SeniorLivingFacility.objects.get(id=facility)
+    return facility
+
+
+def _send_realtime_message(facility: SeniorLivingFacility, content: SeniorLivingFacilityContent):
+    """
+    Send the `content` to all residents of the `facility` as a real-time message
+    :param facility:
+    :param content:
+    :return:
+    """
+
+    rule = content.delivery_rule
+    channel = User.get_facility_channel(facility.facility_id)
+    pusher_client.trigger(channel,
+                          'injectable_content',
+                          {
+                              'url': content.audio_url,
+                              'hash': content.text_content_hash,  # to avoid repetition
+                              'start': rule.start.timestamp(),
+                              'end': rule.end.timestamp(),
+                          })
+
+
+def deliver_daily_calendar(facility: Union[SeniorLivingFacility, int, str]):
+    facility = _get_facility(facility)
 
     events_summary = facility.today_events_summary_in_text()
-
     content = SeniorLivingFacilityContent.find(delivery_type=ContentDeliveryRule.TYPE_INJECTABLE,
                                                start=time_today_in_tz(facility.timezone, 1, 0),
                                                end=time_today_in_tz(facility.timezone, 16, 0),
@@ -37,20 +61,35 @@ def deliver_daily_calendar(facility: Union[SeniorLivingFacility, int, str]):
                                                content_type=SeniorLivingFacilityContent.TYPE_DAILY_CALENDAR,
                                                text_content=events_summary)
 
-    rule = content.delivery_rule
+    _send_realtime_message(facility, content)
 
-    # todo Save to an API-oriented serve-able location for querying content existence
 
-    channel = User.get_facility_channel(facility.facility_id)
+def deliver_upcoming_hourly_events(facility: Union[SeniorLivingFacility, int, str]):
+    facility = _get_facility(facility)
 
-    pusher_client.trigger(channel,
-                          'injectable_content',
-                          {
-                              'url': content.audio_url,
-                              'hash': content.text_content_hash,    # to avoid repetition
-                              'start': rule.start.timestamp(),
-                              'end': rule.end.timestamp(),
-                          })
+    events = facility.get_today_events()
+    hourly_events = events.get('hourly_events')
+
+    for event in hourly_events.get('set'):
+        try:
+            text_content = facility.get_text_for_hourly_event(event, 30)
+        except AssertionError as err:
+            print("There is an Assertion Error for `get_text_for_hourly_event`: {0}".format(err))
+        else:
+            announcement_time = {
+                'start': event['start'] - timedelta(minutes=45),
+                'end': event['start'] - timedelta(minutes=15),
+            }
+
+            content = SeniorLivingFacilityContent.find(delivery_type=ContentDeliveryRule.TYPE_INJECTABLE,
+                                                       start=announcement_time['start'],
+                                                       end=announcement_time['end'],
+                                                       frequency=0,
+                                                       recipient_ids=None,
+                                                       senior_living_facility=facility,
+                                                       content_type=SeniorLivingFacilityContent.TYPE_DAILY_CALENDAR,
+                                                       text_content=text_content)
+            _send_realtime_message(facility, content)
 
 
 def run(fn_name=None, *args):
@@ -58,6 +97,7 @@ def run(fn_name=None, *args):
     Usage: `./manage.py runscript scripts.senior_living_facility.content_delivery.calendar --script-args <function_name> <args_to_function>`
         Examples:
             `./manage.py runscript scripts.senior_living_facility.content_delivery.calendar --script-args deliver_daily_calendar 1`
+            `./manage.py runscript scripts.senior_living_facility.content_delivery.calendar --script-args deliver_upcoming_hourly_events 1`
     """
 
     if fn_name is None:
