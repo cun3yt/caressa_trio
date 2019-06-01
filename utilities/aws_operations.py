311615c73e0289f559db1785e78c1a4ca1cbdf61
Mikail Şeremet <mikail.seremet@gmail.com>
2019-06-01 05:36:08 -0700

    WIP Photo Gallery Image Resizing (#198)

--- utilities/aws_operations.py
+++ utilities/aws_operations.py
@@ -1,6 +1,12 @@
-import boto3
+import os
 
+import boto3
+from PIL import Image
+
+from caressa import settings
 from caressa.settings import S3_RAW_UPLOAD_BUCKET, S3_BUCKET, S3_REGION
+
+from utilities import file_operations as file_ops
 
 
 def aws_url_creator(bucket, file_key):
@@ -23,6 +29,29 @@ def move_file_from_upload_to_prod_bucket(source_file_key, dest_file_key) -> str:
     }
     prod_bucket = s3.Bucket(S3_BUCKET)
     prod_bucket.copy(copy_source, dest_file_key, ExtraArgs={'ACL': 'public-read'})
+    return aws_url_creator(S3_BUCKET, dest_file_key)
+
+
+def resize_photo_from_aws_and_upload_to_prod_bucket(source_file_key, dest_file_key):
+    file_ops.download_to_tmp_from_s3(source_file_key, settings.S3_RAW_UPLOAD_BUCKET)
+
+    file_path = '/tmp/{file_name}'.format(file_name=source_file_key)
+    file_path_to_resize = '{file_path}.resize'.format(file_path=file_path)
+    os.rename(file_path, file_path_to_resize)
+
+    size = 1024, 1024
+    im = Image.open(file_path_to_resize)
+    rgb_im = im.convert("RGB")
+    rgb_im.thumbnail(size, Image.ANTIALIAS)
+    rgb_im.save(file_path, "JPEG")
+
+    s3_client = boto3.client('s3')
+
+    s3_client.upload_file(file_path,
+                          S3_BUCKET,
+                          dest_file_key,
+                          ExtraArgs={'ACL': 'public-read', 'ContentType': 'image/jpeg'})
+
     return aws_url_creator(S3_BUCKET, dest_file_key)
 
 
